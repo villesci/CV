0837b60899da5cabb78f6ed98e0a5f43a335b43a
Andrew Villeneuve <andrewrvilleneuve@gmail.com>
2022-06-30 12:41:24 -0400

    Initial commit

---
+++ orcid_authprep.R
@@ -0,0 +1,19 @@
+# copy/paste your client ID from https://orcid.org/developer-tools
+orcid_client_id <- "APP-MHG77OW9C4KXUPF6"
+
+# copy/paste your client secret from https://orcid.org/developer-tools
+orcid_client_secret <- "ab5d96c6-ba20-4b9f-8002-2f8d731f095a"
+
+orcid_request <- httr::POST(url  = "https://orcid.org/oauth/token",
+                      config = httr::add_headers(`Accept` = "application/json",
+                                           `Content-Type` = "application/x-www-form-urlencoded"),
+                      body = list(grant_type = "client_credentials",
+                                  scope = "/read-public",
+                                  client_id = orcid_client_id,
+                                  client_secret = orcid_client_secret),
+                      encode = "form")
+
+orcid_response <- httr::content(orcid_request)
+print(orcid_response$access_token)
+
+usethis::edit_r_environ()
---
+++ writes_education_yaml.R
@@ -0,0 +1,21 @@
+
+my_orcid<-"0000-0001-7303-5931"
+works<-orcid_works(my_orcid)
+cites<-orcid_citations(my_orcid,cr_format="citeproc-json")
+edu<-orcid_educations(my_orcid)
+edulist <- purrr::map(edu[[my_orcid]][["affiliation-group"]][["summaries"]], function(x) {
+  list(
+    name = paste0(snakecase::to_snake_case(paste(x[["education-summary.organization.name"]], x[["education-summary.end-date.year.value"]]))),
+    type = "degree",
+    title = x[["education-summary.role-title"]],
+    university = x[["education-summary.organization.name"]],
+    department = x[["education-summary.department-name"]],
+    location  = paste0(x[["education-summary.organization.address.city"]], ", ", x[["education-summary.organization.address.region"]]),
+    date = paste(
+      x[["education-summary.end-date.year.value"]],
+      x[["education-summary.end-date.month.value"]],
+      x[["education-summary.end-date.day.value"]],
+      sep = "-"))
+})
+names(edulist) <- purrr::map_chr(edulist, "name")
+yaml::write_yaml(edulist, here::here("education.yaml"), handlers = list(Date = function(x) as.character (x)))
